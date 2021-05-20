1c9f051b4fd1698e8609baaed4f6fad6fa28bab1
matheusdenck <65082832+matheusdenck@users.noreply.github.com>
2021-05-20 09:38:00 -0300

    Initial commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes',
+      home: HomePage(),
+    );
+  }
+}
