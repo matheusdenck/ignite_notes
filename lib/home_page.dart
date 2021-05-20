c8fe4ee41434f0576077e9484eca7055448a76ec
matheusdenck <65082832+matheusdenck@users.noreply.github.com>
2021-05-20 09:46:25 -0300

    .

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Teste 1'),
+            SizedBox(
+              height: 30,
+            ),
+            Text('Teste 2'),
+          ],
+        ),
+      ),
     );
   }
 }
